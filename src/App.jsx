b72361e50522e77c909552ba2f2e637234a02213
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2023-09-12 00:02:36 +0530

    full responsive

--- src/App.jsx
+++ src/App.jsx
@@ -39,48 +39,6 @@ function App() {
     setParts(updatedParts);
   };
 
-  // const splitParagraph = () => {
-  //   const paragraphLength = inputText.split(" ");
-  //   const numParts = Math.ceil(paragraphLength.length / wordLengthPart);
-  //   const partsArray = [];
-
-  //   for (let i = 0; i < numParts; i++) {
-  //     const start = i * wordLengthPart;
-  //     const end = (i + 1) * wordLengthPart;
-  //     const chunkWords = paragraphLength.slice(start, end);
-
-  //     let content = {
-  //       text: "",
-  //       hidden: true,
-  //     };
-
-  //     if (i === numParts - 1) {
-  //       content.text = `[START PART ${i + 1}/${numParts}]\n${chunkWords.join(
-  //         " "
-  //       )}\n[END PART ${
-  //         i + 1
-  //       }/${numParts}]\nALL PARTS SENT. Now you can continue processing the request.`;
-  //     } else {
-  //       content.text = `Do not answer yet. This is just another part of the text I want to send you. Just receive and acknowledge as "Part ${
-  //         i + 1
-  //       }/${numParts} received" and wait for the next part.`;
-  //       content.text += `\n[START PART ${i + 1}/${numParts}]\n${chunkWords.join(
-  //         " "
-  //       )}\n[END PART ${i + 1}/${numParts}]`;
-  //       content.text += `\nRemember not answering yet. Just acknowledge you received this part with the message "Part ${
-  //         i + 1
-  //       }/${numParts} received" and wait for the next part.`;
-  //     }
-
-  //     partsArray.push(content);
-  //   }
-
-  //   setParts(partsArray);
-  //   setShowParts(true);
-  // };
-
-  //
-
   const splitParagraph = () => {
     try {
       if (!inputText.trim()) {
@@ -134,135 +92,74 @@ function App() {
     }
   };
 
-  // return (
-  //   <div className="flex flex-col w-full h-screen lg:flex-row bg-purple-50">
-  //     <div className="w-full px-4 pt-4 m-4 bg-white lg:m-6 lg:pt-8 lg:px-10 lg:w-1/2 rounded-3xl">
-  //       <h1 className="mb-4 text-2xl font-bold text-purple-800 underline underline-offset-8">
-  //         PROMPT SPLITTER
-  //       </h1>
-  //       <textarea
-  //         className="block w-full p-3 bg-white border border-purple-300 shadow-sm lg:h-96 h-96 rounded-xl placeholder-italic placeholder-text-slate-400 focus:outline-none focus:border-purple-500 focus:ring-purple-500 focus:ring-1 sm:text-sm"
-  //         placeholder="Enter your extensive prompt..."
-  //         value={inputText}
-  //         onChange={handleInputChange}
-  //       ></textarea>
-  //       <p className="mt-2 text-sm text-purple-600">
-  //         Prompt Length: {inputText.length} characters.
-  //       </p>
-  //       <p className="mt-2 text-sm text-black">
-  //         Open-source tool can safely process prompt up to around{" "}
-  //         <span className="text-base font-bold text-purple-700">
-  //           15,000 characters per request.
-  //         </span>
-  //       </p>
-  //       <div className="flex justify-between">
-  //         <div className="mt-4">
-  //           <label className="text-purple-800">
-  //             Words Per-Part :
-  //             <input
-  //               type="number"
-  //               className="p-2 ml-4 border border-purple-300 rounded-md focus:outline-none focus:border-purple-500 focus:ring-purple-500 focus:ring-2 sm:text-sm"
-  //               value={wordLengthPart}
-  //               onChange={handleWordLengthChange}
-  //             />
-  //           </label>
-  //         </div>
-  //         <button
-  //           className="px-4 py-2 mt-4 text-white bg-purple-600 rounded-md hover:bg-purple-700"
-  //           onClick={splitParagraph}
-  //         >
-  //           Split Prompt
-  //         </button>
-  //       </div>
-  //     </div>
-  //     <div className="w-full m-3 lg:w-1/2 ">
-  //       {showParts && (
-  //         <div className="mt-6">
-  //           <h2 className="mb-2 text-2xl font-semibold text-purple-800">
-  //             Generated Parts ({parts.length})
-  //           </h2>
-  //           {parts.map((part, index) => (
-  //             <div
-  //               key={index}
-  //               className="p-4 mt-4 bg-white rounded-lg shadow-lg "
-  //             >
-  //               <p
-  //                 className="text-purple-800 cursor-pointer"
-  //                 onClick={() => toggleChunk(index)}
-  //               >
-  //                 {part.hidden
-  //                   ? `Show Part ${index + 1}`
-  //                   : `Hide Part ${index + 1}`}
-  //               </p>
-  //               {!part.hidden && (
-  //                 <div className="">
-  //                   <p className="mt-2 text-purple-800 ">{part.text}</p>
-  //                   <p className="mt-2 text-sm text-purple-600">
-  //                     Character Count: {part.text.length}
-  //                   </p>
-  //                 </div>
-  //               )}
-  //               <button
-  //                 className="px-4 py-2 mt-2 text-white bg-purple-600 rounded-md hover:bg-purple-700"
-  //                 onClick={() => copyToClipboard(part.text)}
-  //               >
-  //                 Copy
-  //               </button>
-  //             </div>
-  //           ))}
-  //         </div>
-  //       )}
-  //     </div>
-  //   </div>
-  // );
-
   return (
-    <div className="flex flex-col w-full h-full p-4 bg-purple-100 lg:p-6 lg:min-h-screen lg:flex-row fluid ">
-      <div className="w-full p-4 bg-white lg:p-6 lg:w-1/3 rounded-3xl">
-        <h1 className="mb-4 text-2xl font-bold text-center text-purple-800 underline lg:text-left underline-offset-8">
-          PROMPT SPLITTER
-        </h1>
-        <textarea
-          className="block w-full p-3 bg-white border border-purple-300 shadow-sm lg:h-full h-96 rounded-xl placeholder-italic placeholder-text-slate-400 focus:outline-none focus:border-purple-500 focus:ring-purple-500 focus:ring-1 sm:text-sm"
-          placeholder="Enter your extensive prompt..."
-          // className="w-full px-2"
-          value={inputText}
-          onChange={handleInputChange}
-        ></textarea>
-        <p>Prompt Length: {inputText.length} characters.</p>
-        <p>
-          Open-source tool can safely process prompt up to around{" "}
-          <span>15,000 characters per request.</span>
-        </p>
-        <div>
-          <div>
-            <label>
-              Words Per-Part:
+    <div className="flex flex-col w-full h-auto p-4 bg-purple-100 lg:p-6 lg:min-h-screen lg:flex-row fluid ">
+      <div className="flex flex-col justify-between w-full p-4 bg-white lg:p-6 lg:w-1/3 rounded-3xl">
+        <div className="h-full">
+          <h1 className="mb-4 text-2xl font-bold text-center text-purple-800 underline lg:text-left underline-offset-8">
+            PROMPT SPLITTER
+          </h1>
+          <textarea
+            className="block w-full p-2 bg-white border-2 border-purple-300 shadow-sm lg:h-3/4 h-96 rounded-xl placeholder:italic placeholder:text-slate-400 placeholder:text-lg focus:outline-none focus:border-purple-500 focus:ring-purple-500 focus:ring-1 sm:text-sm"
+            placeholder="Enter your extensive prompt..."
+            value={inputText}
+            onChange={handleInputChange}
+          ></textarea>
+          <p className="mt-2 text-sm text-purple-600">
+            Prompt Length: {inputText.length} characters.
+          </p>
+          <p className="mt-4 text-sm text-black">
+            Open-source tool can safely process prompt up to around <br />
+            <span className="font-bold text-purple-600">
+              15,000 characters per request.
+            </span>
+          </p>
+        </div>
+        <div className="flex justify-between mb-2">
+          <div className="">
+            <label className="font-semibold text-purple-800">
+              Words Per-Part :
               <input
+                className="w-24 p-2 mt-1 ml-1 border border-purple-300 rounded-lg lg:ml-2 focus:outline-none focus:border-purple-500 focus:ring-purple-500 focus:ring-2 sm:text-sm"
                 type="number"
                 value={wordLengthPart}
                 onChange={handleWordLengthChange}
               />
             </label>
           </div>
-          <button onClick={splitParagraph}>Split Prompt</button>
+          <button
+            className="px-2 py-1 text-white bg-purple-600 rounded-md hover:bg-purple-500"
+            onClick={splitParagraph}
+          >
+            Split Prompt
+          </button>
         </div>
       </div>
-      <div className="w-full p-4 bg-white lg:p-6 lg:w-2/3 rounded-3xl">
+      <div className="w-full p-0 lg:p-4 lg:w-2/3 rounded-3xl">
         {showParts && (
-          <div>
-            <h2>Generated Parts ({parts.length})</h2>
+          <div className="px-0 mt-4 lg:mt-0 lg:px-4">
+            <h2 className="mb-2 text-xl font-semibold text-purple-800 lg:text-2xl">
+              Generated Parts ({parts.length})
+            </h2>
             {parts.map((part, index) => (
-              <div key={index}>
-                <p onClick={() => toggleChunk(index)}>
+              <div
+                key={index}
+                className="p-3 mt-2 bg-white rounded-lg shadow-lg lg:p-4 lg:mt-4 "
+              >
+                <p
+                  onClick={() => toggleChunk(index)}
+                  className="text-purple-800 cursor-pointer"
+                >
                   {part.hidden
                     ? `Show Part ${index + 1}`
                     : `Hide Part ${index + 1}`}
                 </p>
                 {!part.hidden && (
                   <div>
-                    <p>{part.text}</p>
-                    <p>Character Count: {part.text.length}</p>
+                    <p className="mt-2 text-purple-800 ">{part.text}</p>
+                    <p className="mt-2 text-sm text-purple-600">
+                      Character Count: {part.text.length}
+                    </p>
                   </div>
                 )}
                 <button
@@ -270,7 +167,7 @@ function App() {
                     copyToClipboard(part.text);
                     handleElementClick(index);
                   }}
-                  className={`px-4 py-2 mt-2 text-white rounded-md hover:bg-purple-700 ${
+                  className={`px-4 py-2 mt-2 text-white rounded-md hover:bg-purple-600 ${
                     clickedIndex[index] ? "bg-gray-400" : "bg-purple-600"
                   }`}
                 >
